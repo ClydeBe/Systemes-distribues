76584cf209c645c113764ef1ac9e5d5a85db6cef
Loick21 <lcknzenang@gmail.com>
2020-10-14 18:09:33 +0200

    Bootstrap | JSTL

---
+++ src/fr/zcorp/servlets/Reseau.java
@@ -0,0 +1,27 @@
+package fr.zcorp.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Reseau")
+public class Reseau extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Reseau() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher("/WEB-INF/reseau.jsp").forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
