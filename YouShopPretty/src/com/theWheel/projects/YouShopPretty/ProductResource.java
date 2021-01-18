33c66ffb140905abe182fa2ae5d99b5ba939b379
ClydeBe <cbogne2019@gmail.com>
2021-01-18 16:12:56 +0100

    Error's descriptions

--- YouShopPretty/src/com/theWheel/projects/YouShopPretty/ProductResource.java
+++ YouShopPretty/src/com/theWheel/projects/YouShopPretty/ProductResource.java
@@ -64,7 +64,7 @@ public class ProductResource {
 		productRepository.create(p);
 		if(productRepository.errors.isEmpty())
 			return Response.status(Status.CREATED).build();
-		return Response.status(Status.EXPECTATION_FAILED).build();
+		return Response.status(Status.EXPECTATION_FAILED).entity(productRepository.errors).build();
 	}
 	
 	@PUT
@@ -72,7 +72,7 @@ public class ProductResource {
 		productRepository.update(p);
 		if(productRepository.errors.isEmpty())
 			return Response.status(Status.OK).build();
-		return Response.status(Status.EXPECTATION_FAILED).build();
+		return Response.status(Status.EXPECTATION_FAILED).entity(productRepository.errors).build();
 	}
 	
 	@DELETE
@@ -81,6 +81,6 @@ public class ProductResource {
 		productRepository.delete(p);
 		if(productRepository.errors.isEmpty())
 			return Response.status(Status.OK).build();
-		return Response.status(Status.EXPECTATION_FAILED).build();
+		return Response.status(Status.EXPECTATION_FAILED).entity(productRepository.errors).build();
 	}
 }
