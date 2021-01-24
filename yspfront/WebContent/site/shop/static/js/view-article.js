7d9d7fde409e1d4eb7200933c4dc67f587d131f5
Loick21 <lcknzenang@gmail.com>
2021-01-24 11:02:12 +0100

    Login Account ViewArticle front

--- yspfront/WebContent/site/shop/static/js/view-article.js
+++ yspfront/WebContent/site/shop/static/js/view-article.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
-    const domain = "http://localhost:80/YouShopPretty/webapi/";
+    const domain = "http://localhost:86/YouShopPretty/webapi/";
     const products = `products/${id}`;
 
     $.ajax({
@@ -12,10 +12,11 @@ $(document).ready(function(){
         type: 'get',
         success: function(response){
             //console.log(response)
-            let {price, name, images} = response;
+            let {price, name, imageLink} = response;
             $(".price").html(`${price}€`); 
             $(".item-title").html(`${name}`);
-            $("#article_image").attr("src",images);
+            $("#article_image").attr("src",imageLink);
+            $("#article-name").append(`${name}`);
         },
         error: function(){
             console.log("error AJAX GET");
